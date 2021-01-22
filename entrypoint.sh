164384278737962ca776257d7be87860552f6575
Servílio Assis <servilio.assis@taesa.com.br>
2021-01-22 10:13:41 -0300

    simplifying to build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,11 @@
 #!/bin/sh
 
-testcommand="$INPUT_TESTCOMMAND"
-mocha="$INPUT_MOCHA"
-working_dir="$INPUT_WORKING_DIRECTORY"
-
-if [ -n "$working_dir" ]; then
-    cd $working_dir
-fi
-
-echo "\n**Auditing Packages**\n"
-npm audit
-
 echo "\n**Installing Angular CLI**\n"
-npm install -g @angular/cli --latest
-ng config -g cli.warnings.versionMismatch false
 
-if [ "$mocha" = "true" ]; then
-    npm i mocha
-fi
-
-echo "\n**Linting Code**\n"
-ng lint
+npm install -g @angular/cli@9.0.4
 
 echo "\n**Installing Dependencies**\n"
 npm install
 
 echo "\n**Building Project**\n"
 ng build --prod
-
-echo "\n**Running Unit Tests**\n"
-npm $testcommand
